cfff7312ccd14a5fb9bd46198d421609b1fca33f
gevla <viktorboyadjiev@gmail.com>
2023-09-23 17:54:07 +0200

    added classes and minimum speed

--- colors.js
+++ colors.js
@@ -4,33 +4,38 @@ const B = 2;
 
 const X = 0;
 const Y = 1;
+export class Colors {
+  rgb(str) {
+    return [
+      parseInt(str.slice(0, 2), 16),
+      parseInt(str.slice(2, 4), 16),
+      parseInt(str.slice(4, 6), 16),
+    ];
+  }
+  lerp(a, b, alpha) {
+    return a + alpha * (b - a);
+  }
 
-export function rgb(str) {
-  return [
-    parseInt(str.slice(0, 2), 16),
-    parseInt(str.slice(2, 4), 16),
-    parseInt(str.slice(4, 6), 16),
-  ];
-}
-export function lerp(a, b, alpha) {
-  return a + alpha * (b - a);
-}
-
-export function zip(a, b) {
-  return a.map((e, i) => [e, b[i]]);
-}
-export function lerpColor(a, b, alpha) {
-  return zip(a, b).map((x) => lerp(x[0], x[1], alpha));
-}
-export function setColor(color, ctx) {
-  ctx.fillStyle =
-    "rgba(" + color[R] + ", " + color[G] + ", " + color[B] + ", 1)";
-}
-export function getPaddleColor(powershotness) {
-  return lerpColor(rgb("ffffff"), rgb("ff0000"), powershotness);
-}
-export function fillCircle(pos, radius, ctx) {
-  ctx.beginPath();
-  ctx.arc(pos[X], pos[Y], radius, 0, Math.PI * 2, false);
-  ctx.fill();
+  zip(a, b) {
+    return a.map((e, i) => [e, b[i]]);
+  }
+  lerpColor(a, b, alpha) {
+    return this.zip(a, b).map((x) => this.lerp(x[0], x[1], alpha));
+  }
+  setColor(color, ctx) {
+    ctx.fillStyle =
+      "rgba(" + color[R] + ", " + color[G] + ", " + color[B] + ", 1)";
+  }
+  getPaddleColor(powershotness) {
+    return this.lerpColor(
+      this.rgb("ffffff"),
+      this.rgb("ff0000"),
+      powershotness
+    );
+  }
+  fillCircle(pos, radius, ctx) {
+    ctx.beginPath();
+    ctx.arc(pos[X], pos[Y], radius, 0, Math.PI * 2, false);
+    ctx.fill();
+  }
 }
--- main.js
+++ main.js
@@ -1,12 +1,6 @@
 "use strict";
 
-import {
-  rgb,
-  lerpColor,
-  setColor,
-  getPaddleColor,
-  fillCircle,
-} from "./colors.js";
+import { Colors } from "./colors.js";
 
 const canvas = document.getElementById("canvasId");
 const ctx = canvas.getContext("2d");
@@ -37,158 +31,160 @@ let spin;
 
 let oldBallPositions = Array(OLD_BALL_POSITION_COUNT).fill([ballX, ballY]);
 let oldBallPositionIndex = 0;
-
-function resetGame() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-
-  leftPaddleY = rightPaddleY = (canvas.height - PADDLE_HEIGHT) / 2;
-
-  ballX = canvas.width / 2;
-  ballY = canvas.height / 2;
-  const startAngle = Math.random() * 2 * Math.PI;
-  velocityX = Math.cos(startAngle) / 2;
-  velocityY = Math.sin(startAngle) / 2;
-
-  leftPowershotness = 0;
-  rightPowershotness = 0;
-
-  spin = 0;
-}
-
-resetGame();
-
 let previousTime = 0;
 
-function draw() {
-  setColor(rgb("202833"), ctx);
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+const colors = new Colors();
 
-  for (let i = 0; i < OLD_BALL_POSITION_COUNT; i++) {
-    setColor(
-      lerpColor(
-        rgb("202833"),
-        rgb("ee8888"),
-        (i + 1) / OLD_BALL_POSITION_COUNT
-      ),
-      ctx
-    );
-    const positionIndex = (oldBallPositionIndex + i) % OLD_BALL_POSITION_COUNT;
-    fillCircle(oldBallPositions[positionIndex], BALL_RADIUS, ctx);
-  }
+class Game {
+  resetGame() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-  setColor(rgb("ffffff"), ctx);
-  fillCircle([ballX, ballY], BALL_RADIUS, ctx);
+    leftPaddleY = rightPaddleY = (canvas.height - PADDLE_HEIGHT) / 2;
 
-  setColor(getPaddleColor(leftPowershotness), ctx);
-  ctx.fillRect(0, leftPaddleY, PADDLE_WIDTH, PADDLE_HEIGHT);
-  setColor(getPaddleColor(rightPowershotness), ctx);
-  ctx.fillRect(
-    canvas.width - PADDLE_WIDTH,
-    rightPaddleY,
-    PADDLE_WIDTH,
-    PADDLE_HEIGHT
-  );
+    ballX = canvas.width / 2;
+    ballY = canvas.height / 2;
+    const startAngle = Math.random() * 2 * Math.PI;
+    velocityX = Math.cos(startAngle) / 2;
+    velocityY = Math.sin(startAngle) / 2;
 
-  setColor(rgb("ffffff"), ctx);
-  ctx.font = "30px Arial";
-  ctx.fillText(`${leftScore} : ${rightScore}`, 50, 50);
-  ctx.fillText(velocityX, 250, 50);
-}
-
-function paddleMove(deltaTime) {
-  const deltaTimeMultiplier = 0.0005;
-
-  if (pressedKeys.has("KeyD")) {
-    leftPowershotness = Math.min(
-      leftPowershotness + deltaTime * deltaTimeMultiplier,
-      1
-    );
-  } else {
-    if (pressedKeys.has("KeyW")) leftPaddleY -= deltaTime * PIXELS_PER_MS;
-    if (pressedKeys.has("KeyS")) leftPaddleY += deltaTime * PIXELS_PER_MS;
-  }
-
-  if (pressedKeys.has("ArrowLeft")) {
-    rightPowershotness = Math.min(
-      rightPowershotness + deltaTime * deltaTimeMultiplier,
-      1
-    );
-  } else {
-    if (pressedKeys.has("ArrowUp")) rightPaddleY -= deltaTime * PIXELS_PER_MS;
-    if (pressedKeys.has("ArrowDown")) rightPaddleY += deltaTime * PIXELS_PER_MS;
-  }
-  leftPaddleY = Math.min(
-    Math.max(leftPaddleY, 0),
-    canvas.height - PADDLE_HEIGHT
-  );
-  rightPaddleY = Math.min(
-    Math.max(rightPaddleY, 0),
-    canvas.height - PADDLE_HEIGHT
-  );
-}
-
-function onHit(oldLeftPaddleY, oldRightPaddleY) {
-  let ballAcceleration = 0.4;
-  if (
-    ballX - BALL_RADIUS <= PADDLE_WIDTH &&
-    ballY - BALL_RADIUS < leftPaddleY + PADDLE_HEIGHT &&
-    ballY + BALL_RADIUS > leftPaddleY &&
-    velocityX < 0
-  ) {
-    HIT_SOUND.volume = Math.max(leftPowershotness, 0.1);
-    HIT_SOUND.play();
-    spin -= Math.sign(leftPaddleY - oldLeftPaddleY);
-    velocityX = -velocityX + ballAcceleration + leftPowershotness;
     leftPowershotness = 0;
-  }
-  if (
-    ballX + BALL_RADIUS >= canvas.width - PADDLE_WIDTH &&
-    ballY - BALL_RADIUS < rightPaddleY + PADDLE_HEIGHT &&
-    ballY + BALL_RADIUS > rightPaddleY &&
-    velocityX > 0
-  ) {
-    HIT_SOUND.volume = Math.max(rightPowershotness, 0.1);
-    HIT_SOUND.play();
-    spin -= Math.sign(rightPaddleY - oldRightPaddleY);
-    velocityX = -velocityX - ballAcceleration - rightPowershotness;
     rightPowershotness = 0;
+
+    spin = 0;
+  }
+  draw() {
+    colors.setColor(colors.rgb("202833"), ctx);
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i < OLD_BALL_POSITION_COUNT; i++) {
+      colors.setColor(
+        colors.lerpColor(
+          colors.rgb("202833"),
+          colors.rgb("ee8888"),
+          (i + 1) / OLD_BALL_POSITION_COUNT
+        ),
+        ctx
+      );
+      const positionIndex =
+        (oldBallPositionIndex + i) % OLD_BALL_POSITION_COUNT;
+      colors.fillCircle(oldBallPositions[positionIndex], BALL_RADIUS, ctx);
+    }
+
+    colors.setColor(colors.rgb("ffffff"), ctx);
+    colors.fillCircle([ballX, ballY], BALL_RADIUS, ctx);
+
+    colors.setColor(colors.getPaddleColor(leftPowershotness), ctx);
+    ctx.fillRect(0, leftPaddleY, PADDLE_WIDTH, PADDLE_HEIGHT);
+    colors.setColor(colors.getPaddleColor(rightPowershotness), ctx);
+    ctx.fillRect(
+      canvas.width - PADDLE_WIDTH,
+      rightPaddleY,
+      PADDLE_WIDTH,
+      PADDLE_HEIGHT
+    );
+
+    colors.setColor(colors.rgb("ffffff"), ctx);
+    ctx.font = "30px Arial";
+    ctx.fillText(`${leftScore} : ${rightScore}`, 50, 50);
+    ctx.fillText(velocityX, 250, 50);
+  }
+  paddleMove(deltaTime) {
+    const deltaTimeMultiplier = 0.0005;
+
+    if (pressedKeys.has("KeyD")) {
+      leftPowershotness = Math.min(
+        leftPowershotness + deltaTime * deltaTimeMultiplier,
+        1
+      );
+    } else {
+      if (pressedKeys.has("KeyW")) leftPaddleY -= deltaTime * PIXELS_PER_MS;
+      if (pressedKeys.has("KeyS")) leftPaddleY += deltaTime * PIXELS_PER_MS;
+    }
+
+    if (pressedKeys.has("ArrowLeft")) {
+      rightPowershotness = Math.min(
+        rightPowershotness + deltaTime * deltaTimeMultiplier,
+        1
+      );
+    } else {
+      if (pressedKeys.has("ArrowUp")) rightPaddleY -= deltaTime * PIXELS_PER_MS;
+      if (pressedKeys.has("ArrowDown"))
+        rightPaddleY += deltaTime * PIXELS_PER_MS;
+    }
+    leftPaddleY = Math.min(
+      Math.max(leftPaddleY, 0),
+      canvas.height - PADDLE_HEIGHT
+    );
+    rightPaddleY = Math.min(
+      Math.max(rightPaddleY, 0),
+      canvas.height - PADDLE_HEIGHT
+    );
+  }
+  onHit(oldLeftPaddleY, oldRightPaddleY) {
+    let ballAcceleration = 0.4;
+    if (
+      ballX - BALL_RADIUS <= PADDLE_WIDTH &&
+      ballY - BALL_RADIUS < leftPaddleY + PADDLE_HEIGHT &&
+      ballY + BALL_RADIUS > leftPaddleY &&
+      velocityX < 0
+    ) {
+      HIT_SOUND.volume = Math.max(leftPowershotness, 0.1);
+      HIT_SOUND.play();
+      spin -= Math.sign(leftPaddleY - oldLeftPaddleY);
+      velocityX = -velocityX + ballAcceleration + leftPowershotness;
+      leftPowershotness = 0;
+    }
+    if (
+      ballX + BALL_RADIUS >= canvas.width - PADDLE_WIDTH &&
+      ballY - BALL_RADIUS < rightPaddleY + PADDLE_HEIGHT &&
+      ballY + BALL_RADIUS > rightPaddleY &&
+      velocityX > 0
+    ) {
+      HIT_SOUND.volume = Math.max(rightPowershotness, 0.1);
+      HIT_SOUND.play();
+      spin -= Math.sign(rightPaddleY - oldRightPaddleY);
+      velocityX = -velocityX - ballAcceleration - rightPowershotness;
+      rightPowershotness = 0;
+    }
+  }
+  onCornerHit() {
+    const wallVelocityMultiplier = 0.7;
+    const wallSpinMultiplier = 0.3;
+    if (ballY <= BALL_RADIUS && velocityY < 0) {
+      velocityX -= spin * wallVelocityMultiplier;
+      spin *= wallSpinMultiplier;
+      velocityY *= -1;
+    }
+    if (ballY >= canvas.height - BALL_RADIUS && velocityY > 0) {
+      velocityX += spin * wallVelocityMultiplier;
+      spin *= wallSpinMultiplier;
+      velocityY *= -1;
+    }
+  }
+  getDeltaTime(time) {
+    let deltaTime = time - (previousTime ?? time);
+    if (deltaTime > 500) deltaTime = 0;
+    previousTime = time;
+    return deltaTime;
+  }
+
+  checkGameState() {
+    if (ballX < 0) {
+      rightScore++;
+      this.resetGame();
+    } else if (ballX > canvas.width) {
+      leftScore++;
+      this.resetGame();
+    }
   }
 }
 
-function onCornerHit() {
-  const wallVelocityMultiplier = 0.7;
-  const wallSpinMultiplier = 0.3;
-  if (ballY <= BALL_RADIUS && velocityY < 0) {
-    velocityX -= spin * wallVelocityMultiplier;
-    spin *= wallSpinMultiplier;
-    velocityY *= -1;
-  }
-  if (ballY >= canvas.height - BALL_RADIUS && velocityY > 0) {
-    velocityX += spin * wallVelocityMultiplier;
-    spin *= wallSpinMultiplier;
-    velocityY *= -1;
-  }
-}
-function getDeltaTime(time) {
-  let deltaTime = time - (previousTime ?? time);
-  if (deltaTime > 500) deltaTime = 0;
-  previousTime = time;
-  return deltaTime;
-}
-
-function checkGameState() {
-  if (ballX < 0) {
-    rightScore++;
-    resetGame();
-  } else if (ballX > canvas.width) {
-    leftScore++;
-    resetGame();
-  }
-}
+const game = new Game();
+game.resetGame();
 
 function onFrame(time) {
-  let deltaTime = getDeltaTime(time);
+  let deltaTime = game.getDeltaTime(time);
 
   const oldLeftPaddleY = leftPaddleY;
   const oldRightPaddleY = rightPaddleY;
@@ -196,24 +192,24 @@ function onFrame(time) {
   oldBallPositions[oldBallPositionIndex] = [ballX, ballY];
   oldBallPositionIndex = (oldBallPositionIndex + 1) % OLD_BALL_POSITION_COUNT;
 
-  paddleMove(deltaTime);
+  game.paddleMove(deltaTime);
   if (velocityX > 0 && velocityX < 0.3) velocityX = 0.3;
   if (velocityX < 0 && velocityX > -0.3) velocityX = -0.3;
   ballX += velocityX * deltaTime;
   ballY += velocityY * deltaTime;
 
-  onCornerHit();
+  game.onCornerHit();
 
-  onHit(oldLeftPaddleY, oldRightPaddleY);
+  game.onHit(oldLeftPaddleY, oldRightPaddleY);
 
   velocityX -= (deltaTime / 2000) * velocityX * Math.abs(velocityX);
   velocityY -=
     (deltaTime / 2000) * (velocityY * Math.abs(velocityY) - spin * 2);
   spin -= (deltaTime / 200) * spin * 0.5 * Math.abs(spin);
 
-  checkGameState();
+  game.checkGameState();
 
-  draw();
+  game.draw();
 
   window.requestAnimationFrame(onFrame);
 }
