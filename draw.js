baa5d40a8f726284fc092a7cbd1bdb3b5864cabd
gevla <viktorboyadjiev@gmail.com>
2023-09-25 12:29:00 +0200

    added dash cooldown and strokeRect

--- draw.js
+++ draw.js
@@ -10,12 +10,23 @@ export default class Canvas {
     this.ctx.fillStyle =
       "rgba(" + color[R] + ", " + color[G] + ", " + color[B] + ", 1)";
   }
+  #setStrokeColor(color) {
+    this.ctx.strokeStyle =
+      "rgba(" + color[R] + ", " + color[G] + ", " + color[B] + ", 1)";
+  }
 
   fillRect(pos, size, color) {
     this.#setColor(color);
     this.ctx.fillRect(pos[X], pos[Y], size[WIDTH], size[HEIGHT]);
   }
-  
+  strokeRect(pos, size, color, thickness) {
+    this.#setStrokeColor(color);
+    this.ctx.beginPath();
+    this.ctx.lineWidth = thickness;
+    this.ctx.rect(pos[X], pos[Y], size[WIDTH], size[HEIGHT]);
+    this.ctx.stroke();
+  }
+
   fillCircle(pos, radius, color) {
     this.#setColor(color);
     this.ctx.beginPath();
--- game.js
+++ game.js
@@ -25,6 +25,8 @@ export default class Game {
   }
 
   reset() {
+    this.dashCooldowns = [0, 0];
+
     this.size = [window.innerWidth, window.innerHeight];
     this.canvas.width = this.size[WIDTH];
     this.canvas.height = this.size[HEIGHT];
@@ -33,7 +35,7 @@ export default class Game {
 
     this.paddleYs = vrep((this.size[HEIGHT] - PADDLE_SIZE[HEIGHT]) / 2, 2);
 
-    this.ballPos = vmul(this.size, vrep(.5, 2));
+    this.ballPos = vmul(this.size, vrep(0.5, 2));
 
     const startAngle = Math.random() * 2 * Math.PI;
     this.ballVelocity = [Math.cos(startAngle) / 2, Math.sin(startAngle) / 2];
@@ -41,6 +43,21 @@ export default class Game {
     this.powershotnesses = [0, 0];
 
     this.spin = 0;
+  }
+
+  drawPaddle(ctx, side, shakeOffset, paddleColor, x) {
+    if (this.dashCooldowns[side] > 0)
+      ctx.strokeRect(
+        vadd([x, this.paddleYs[side]], shakeOffset),
+        vmul(PADDLE_SIZE, [1, this.dashCooldowns[side]]),
+        rgb("F28500"),
+        10
+      );
+    ctx.fillRect(
+      vadd([x, this.paddleYs[side]], shakeOffset),
+      PADDLE_SIZE,
+      paddleColor
+    );
   }
 
   draw(ctx) {
@@ -56,68 +73,121 @@ export default class Game {
 
     ctx.fillCircle(this.ballPos, BALL_RADIUS, rgb("ffffff"));
 
-    const shakeOffsets = this.paddleShakenesses.map(s => [0, 0].map(_ => (Math.random() - .5) * 30 * s));
-
+    const shakeOffsets = this.paddleShakenesses.map((s) =>
+      [0, 0].map((_) => (Math.random() - 0.5) * 30 * s)
+    );
     const paddleColors = this.powershotnesses.map(getPaddleColor);
-    ctx.fillRect(vadd([0, this.paddleYs[LEFT]], shakeOffsets[LEFT]), PADDLE_SIZE, paddleColors[LEFT]);
 
-    const rightPaddlePos = [this.size[WIDTH] - PADDLE_SIZE[WIDTH], this.paddleYs[RIGHT]];
-    ctx.fillRect(vadd(rightPaddlePos, shakeOffsets[RIGHT]), PADDLE_SIZE, paddleColors[RIGHT]);
+    this.drawPaddle(ctx, LEFT, shakeOffsets[LEFT], paddleColors[LEFT], 0);
+    const rightPaddleX = this.size[WIDTH] - PADDLE_SIZE[WIDTH];
+    this.drawPaddle(
+      ctx,
+      RIGHT,
+      shakeOffsets[RIGHT],
+      paddleColors[RIGHT],
+      rightPaddleX
+    );
 
     const centerX = this.size[WIDTH] / 2;
     const SCORE_Y = 60;
     const FONT = "50px Arial";
     ctx.fillText([centerX, SCORE_Y], ":", FONT, rgb("ffffff"), "center");
-    ctx.fillText([centerX - 15, SCORE_Y], this.score[LEFT], FONT, rgb("ffffff"), "right");
-    ctx.fillText([centerX + 15, SCORE_Y], this.score[RIGHT], FONT, rgb("ffffff"), "left");
+    ctx.fillText(
+      [centerX - 15, SCORE_Y],
+      this.score[LEFT],
+      FONT,
+      rgb("ffffff"),
+      "right"
+    );
+    ctx.fillText(
+      [centerX + 15, SCORE_Y],
+      this.score[RIGHT],
+      FONT,
+      rgb("ffffff"),
+      "left"
+    );
   }
 
   movePaddles(pressedKeys, deltaTime) {
-    const c =
-      zipMany([this.powershotnesses, this.paddleYs, ["KeyD", "ArrowLeft"], ["KeyW", "ArrowUp"], ["KeyS", "ArrowDown"]])
-      .map(([powershotness, paddleY, powershotKey, upKey, downKey]) => {
+    const c = zipMany([
+      this.powershotnesses,
+      this.paddleYs,
+      ["KeyD", "ArrowLeft"],
+      ["KeyW", "ArrowUp"],
+      ["KeyS", "ArrowDown"],
+      ["KeyQ", "ArrowRight"],
+      this.dashCooldowns,
+    ]).map(
+      ([
+        powershotness,
+        paddleY,
+        powershotKey,
+        upKey,
+        downKey,
+        dashKey,
+        dashCooldown,
+      ]) => {
         if (pressedKeys.has(powershotKey)) {
-          console.log("power");
           powershotness = Math.min(powershotness + deltaTime * 0.0005, 1);
         } else {
-          if (pressedKeys.has(upKey)) paddleY -= deltaTime * PIXELS_PER_MS;
-          if (pressedKeys.has(downKey)) paddleY += deltaTime * PIXELS_PER_MS;
+          const downness = pressedKeys.has(downKey) - pressedKeys.has(upKey);
+          let deltaY = downness * deltaTime * PIXELS_PER_MS;
+          if (pressedKeys.has(dashKey) && dashCooldown == 0 && downness != 0) {
+            deltaY *= 40;
+            dashCooldown = 1;
+          }
           const maxPaddleY = this.size[HEIGHT] - PADDLE_SIZE[HEIGHT];
-          paddleY = Math.min(Math.max(paddleY, 0), maxPaddleY);
+          paddleY = Math.min(Math.max(paddleY + deltaY, 0), maxPaddleY);
         }
-        return [powershotness, paddleY];
-      });
+        return [powershotness, paddleY, dashCooldown];
+      }
+    );
     // [[lPS, lPY], [rPS, rPY]] => [[lPS, rPS], [lPY, rPY]]
-    this.powershotnesses = c.map(([powershotness, _]) => powershotness);
-    this.paddleYs = c.map(([_, paddleY]) => paddleY);
+    this.powershotnesses = c.map(([powershotness, _1, _2]) => powershotness);
+    this.paddleYs = c.map(([_1, paddleY, _2]) => paddleY);
+    this.dashCooldowns = c.map(([_1, _2, cooldown]) => cooldown);
   }
 
   hitPaddles(oldPaddleYs) {
     const ballAcceleration = 0.4;
     if (
       this.ballPos[X] - BALL_RADIUS <= PADDLE_SIZE[WIDTH] &&
-      this.ballPos[Y] - BALL_RADIUS < this.paddleYs[LEFT] + PADDLE_SIZE[HEIGHT] &&
+      this.ballPos[Y] - BALL_RADIUS <
+        this.paddleYs[LEFT] + PADDLE_SIZE[HEIGHT] &&
       this.ballPos[Y] + BALL_RADIUS > this.paddleYs[LEFT] &&
       this.ballVelocity[X] < 0
     ) {
       this.hitSound.volume = Math.max(this.powershotnesses[LEFT], 0.1);
       this.hitSound.play();
       this.spin -= Math.sign(this.paddleYs[LEFT] - oldPaddleYs[LEFT]);
-      this.ballVelocity = [-this.ballVelocity[X] + ballAcceleration + this.powershotnesses[LEFT], this.ballVelocity[Y]];
-      this.paddleShakenesses = [0.3 + this.powershotnesses[LEFT], this.paddleShakenesses[RIGHT]];
+      this.ballVelocity = [
+        -this.ballVelocity[X] + ballAcceleration + this.powershotnesses[LEFT],
+        this.ballVelocity[Y],
+      ];
+      this.paddleShakenesses = [
+        0.3 + this.powershotnesses[LEFT],
+        this.paddleShakenesses[RIGHT],
+      ];
       this.powershotnesses = [0, this.powershotnesses[RIGHT]];
     }
     if (
       this.ballPos[X] + BALL_RADIUS >= this.size[WIDTH] - PADDLE_SIZE[WIDTH] &&
-      this.ballPos[Y] - BALL_RADIUS < this.paddleYs[RIGHT] + PADDLE_SIZE[HEIGHT] &&
+      this.ballPos[Y] - BALL_RADIUS <
+        this.paddleYs[RIGHT] + PADDLE_SIZE[HEIGHT] &&
       this.ballPos[Y] + BALL_RADIUS > this.paddleYs[RIGHT] &&
       this.ballVelocity[X] > 0
     ) {
       this.hitSound.volume = Math.max(this.powershotnesses[RIGHT], 0.1);
       this.hitSound.play();
       this.spin -= Math.sign(this.paddleYs[RIGHT] - oldPaddleYs[RIGHT]);
-      this.ballVelocity = [-this.ballVelocity[X] - ballAcceleration - this.powershotnesses[RIGHT], this.ballVelocity[Y]];
-      this.paddleShakenesses = [this.paddleShakenesses[LEFT], 0.3 + this.powershotnesses[RIGHT]];
+      this.ballVelocity = [
+        -this.ballVelocity[X] - ballAcceleration - this.powershotnesses[RIGHT],
+        this.ballVelocity[Y],
+      ];
+      this.paddleShakenesses = [
+        this.paddleShakenesses[LEFT],
+        0.3 + this.powershotnesses[RIGHT],
+      ];
       this.powershotnesses = [this.powershotnesses[LEFT], 0];
     }
   }
@@ -132,7 +202,10 @@ export default class Game {
       ];
       this.spin *= wallSpinMultiplier;
     }
-    if (this.ballPos[Y] >= this.size[HEIGHT] - BALL_RADIUS && this.ballVelocity[Y] > 0) {
+    if (
+      this.ballPos[Y] >= this.size[HEIGHT] - BALL_RADIUS &&
+      this.ballVelocity[Y] > 0
+    ) {
       this.ballVelocity = [
         this.ballVelocity[X] + this.spin * wallVelocityMultiplier,
         -this.ballVelocity[Y],
@@ -145,8 +218,16 @@ export default class Game {
     const oldPaddleYs = this.paddleYs;
 
     this.oldBallPositions[this.oldBallPositionIndex] = this.ballPos;
-    this.oldBallPositionIndex = (this.oldBallPositionIndex + 1) % OLD_BALL_POSITION_COUNT;
-    this.paddleShakenesses = this.paddleShakenesses.map(s => Math.max(s - deltaTime / 500, 0));
+    this.oldBallPositionIndex =
+      (this.oldBallPositionIndex + 1) % OLD_BALL_POSITION_COUNT;
+
+    this.paddleShakenesses = this.paddleShakenesses.map((s) =>
+      Math.max(s - deltaTime / 500, 0)
+    );
+
+    this.dashCooldowns = this.dashCooldowns.map((cooldown) =>
+      Math.max(cooldown - deltaTime / 3000, 0)
+    );
 
     this.movePaddles(pressedKeys, deltaTime);
 
@@ -154,15 +235,24 @@ export default class Game {
       this.ballVelocity = [0.3, this.ballVelocity[Y]];
     if (this.ballVelocity[X] < 0 && this.ballVelocity[X] > -0.3)
       this.ballVelocity = [-0.3, this.ballVelocity[Y]];
-    this.ballPos = vadd(this.ballPos, vmul(this.ballVelocity, vrep(deltaTime, 2)));
+    this.ballPos = vadd(
+      this.ballPos,
+      vmul(this.ballVelocity, vrep(deltaTime, 2))
+    );
 
     this.hitTopAndBottom();
 
     this.hitPaddles(oldPaddleYs);
 
     this.ballVelocity = [
-      this.ballVelocity[X] - (deltaTime / 2000) * this.ballVelocity[X] * Math.abs(this.ballVelocity[X]),
-      this.ballVelocity[Y] - (deltaTime / 2000) * (this.ballVelocity[Y] * Math.abs(this.ballVelocity[Y]) - this.spin * 2),
+      this.ballVelocity[X] -
+        (deltaTime / 2000) *
+          this.ballVelocity[X] *
+          Math.abs(this.ballVelocity[X]),
+      this.ballVelocity[Y] -
+        (deltaTime / 2000) *
+          (this.ballVelocity[Y] * Math.abs(this.ballVelocity[Y]) -
+            this.spin * 2),
     ];
     this.spin -= (deltaTime / 200) * this.spin * 0.5 * Math.abs(this.spin);
 
